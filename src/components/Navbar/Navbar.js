3c255feb592583d940dcb21ec6397afd5595cbc0
itsyourtechguy <crronaldo749@gmail.com>
2025-01-05 03:47:07 +0530

    added Resume

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
+import { resume } from "../Resume/Resume";
 import "./Navbar.css";
 
 function Navbar({ setActiveSection }) {
   const [menuOpen, setMenuOpen] = useState(false);
+  const resumeLink = "https://itsyourtechguy.github.io/resume/";
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
@@ -10,7 +12,7 @@ function Navbar({ setActiveSection }) {
 
   const handleLinkClick = (section) => {
     setActiveSection(section);
-    setMenuOpen(false); // Close the menu on link click
+    setMenuOpen(false);
   };
 
   return (
@@ -20,6 +22,15 @@ function Navbar({ setActiveSection }) {
           <li onClick={() => handleLinkClick("about")}>About</li>
           <li onClick={() => handleLinkClick("projects")}>Projects</li>
           <li onClick={() => handleLinkClick("contact")}>Contact</li>
+          <li>
+            <a
+              href={resumeLink}
+              rel="noopener noreferrer"
+              className="resume-link"
+            >
+              Resume
+            </a>
+          </li>
         </ul>
         <div className="menu-toggle" onClick={toggleMenu}>
           &#9776;
